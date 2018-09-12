26b1a8eefc4041b4064ca703174909791f18710d
Benjamin Huang <bhuang@rms.com>
2018-09-11 21:47:02 -0700

    disable auth

--- server/modules/bprecords/routes.js
+++ server/modules/bprecords/routes.js
@@ -3,8 +3,8 @@ import * as BPRecordController from './controller';
 import { requireJwtAuth } from '../../utils/requireJwtAuth';
 
 const routes = new Router();
-
-routes.post('/bprecords/new', requireJwtAuth, BPRecordController.createBPRecord);
-routes.post('/bprecords', requireJwtAuth, BPRecordController.getBPRecords);
+//requireJwtAuth, 
+routes.post('/bprecords/new', BPRecordController.createBPRecord);
+routes.post('/bprecords', BPRecordController.getBPRecords);
 
 export default routes;
