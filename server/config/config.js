4965074fe8aca95de4cdc7e9028084ca2881faa0
Benjamin Huang <bhuang@rms.com>
2018-09-14 17:54:46 -0700

    update

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,5 @@
 const devConfig = {
   MONGO_URI_DEV: 'mongodb://admin:admin1234@ds028559.mlab.com:28559/bp-records-dev',
-  MONGO_URI_PRD: 'mongodb://admin:admin1234@ds028559.mlab.com:28559/bp-records',
   JWT_SECRET: 'JWT_SECRET+JWT_SECRET',
 };
 
